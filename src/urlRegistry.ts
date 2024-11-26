505a304607803bec8c8ebe30c03e0d41201362b6
Dief Bell <dief.bell@grabcad.com>
2024-11-26 09:59:52 +0000

    Using nullish coalescing

--- src/urlRegistry.ts
+++ src/urlRegistry.ts
@@ -3,9 +3,9 @@ import gte from 'semver/functions/gte';
 import lt from 'semver/functions/lt';
 import os from 'os';
 
-const NODE_MIRROR = process.env.NVM_NODEJS_ORG_MIRROR || "https://nodejs.org/dist";
-const IOJS_MIRROR = process.env.NVM_IOJS_ORG_MIRROR || "https://iojs.org/dist";
-const ELECTRON_MIRROR = process.env.ELECTRON_MIRROR || "https://artifacts.electronjs.org/headers/dist";
+const NODE_MIRROR = process.env.NVM_NODEJS_ORG_MIRROR ?? "https://nodejs.org/dist";
+const IOJS_MIRROR = process.env.NVM_IOJS_ORG_MIRROR ?? "https://iojs.org/dist";
+const ELECTRON_MIRROR = process.env.ELECTRON_MIRROR ?? "https://artifacts.electronjs.org/headers/dist";
 
 export const HOME_DIRECTORY = process.env[(os.platform() === "win32") ? "USERPROFILE" : "HOME"] as string;
 
