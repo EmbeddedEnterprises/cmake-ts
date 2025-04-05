02f17e845fb70451732af0fa7ba23e7e2e7b3545
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2025-04-05 02:08:07 -0700

    fix: add colors for the logger

--- src/logger.ts
+++ src/logger.ts
@@ -19,19 +19,19 @@ class Logger {
 
   error(...args: unknown[]) {
     if (this.level >= 0) {
-      console.error("[ERROR cmake-ts]", ...args)
+      console.error("\x1b[31m[ERROR cmake-ts]\x1b[0m", ...args)
     }
   }
 
   warn(...args: unknown[]) {
     if (this.level >= 1) {
-      console.warn("[WARN cmake-ts]", ...args)
+      console.warn("\x1b[33m[WARN cmake-ts]\x1b[0m", ...args)
     }
   }
 
   info(...args: unknown[]) {
     if (this.level >= 2) {
-      console.info("[INFO cmake-ts]", ...args)
+      console.info("\x1b[32m[INFO cmake-ts]\x1b[0m", ...args)
     }
   }
 
@@ -41,13 +41,13 @@ class Logger {
 
   debug(...args: unknown[]) {
     if (this.level >= 3) {
-      console.debug("[DEBUG cmake-ts]", ...args, { level: this.level })
+      console.debug("\x1b[34m[DEBUG cmake-ts]\x1b[0m", ...args, { level: this.level })
     }
   }
 
   trace(...args: unknown[]) {
     if (this.level >= 4) {
-      console.trace("[TRACE cmake-ts]", ...args)
+      console.trace("\x1b[34m[TRACE cmake-ts]\x1b[0m", ...args)
     }
   }
 }
