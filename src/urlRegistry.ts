a964777bf3905d6a1a06ea2773b437ff5c289c96
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2025-04-09 04:02:56 -0700

    fix: fix the node.lib path for arm64

--- src/urlRegistry.ts
+++ src/urlRegistry.ts
@@ -19,8 +19,10 @@ export function getPathsForConfig(config: BuildConfiguration) {
       return {
         externalPath: `${IOJS_MIRROR}/v${config.runtimeVersion}/`,
         winLibs: [
+          // https://iojs.org/dist/v3.3.1/win-x64/iojs.lib
+          // https://iojs.org/dist/v3.3.1/win-x86/iojs.lib
           {
-            dir: config.arch === "x64" ? "win-x64" : "win-x86",
+            dir: config.arch === "x64" || config.arch === "arm64" ? "win-x64" : "win-x86",
             name: `${config.runtime}.lib`,
           },
         ],
@@ -33,7 +35,10 @@ export function getPathsForConfig(config: BuildConfiguration) {
         externalPath: `${ELECTRON_MIRROR}/v${config.runtimeVersion}/`,
         winLibs: [
           {
-            dir: config.arch === "x64" ? "x64" : "",
+            // https://artifacts.electronjs.org/headers/dist/v17.0.0/x64/node.lib
+            // https://artifacts.electronjs.org/headers/dist/v17.0.0/arm64/node.lib
+            // https://artifacts.electronjs.org/headers/dist/v17.0.0/node.lib
+            dir: config.arch === "x64" ? "x64" : config.arch === "arm64" ? "arm64" : "",
             name: "node.lib",
           },
         ],
@@ -52,7 +57,8 @@ function nodePrehistoric(config: BuildConfiguration) {
     externalPath: `${NODE_MIRROR}/v${config.runtimeVersion}/`,
     winLibs: [
       {
-        dir: config.arch === "x64" ? "x64" : "",
+        // https://nodejs.org/dist/v0.9.9/x64/node.lib
+        dir: config.arch === "x64" || config.arch === "arm64" ? "x64" : <never>"",
         name: `${config.runtime}.lib`,
       },
     ],
@@ -65,8 +71,18 @@ function nodeModern(config: BuildConfiguration) {
   return {
     externalPath: `${NODE_MIRROR}/v${config.runtimeVersion}/`,
     winLibs: [
+      // https://nodejs.org/dist/v22.14.0/win-x64/node.lib
+      // https://nodejs.org/dist/v22.14.0/win-x86/node.lib
+      // https://nodejs.org/dist/v22.14.0/win-arm64/node.lib
       {
-        dir: config.arch === "x64" ? "win-x64" : "win-x86",
+        dir:
+          config.arch === "x64"
+            ? "win-x64"
+            : config.arch === "arm64"
+              ? "win-arm64"
+              : config.arch === "ia32"
+                ? "win-x86"
+                : <never>"",
         name: `${config.runtime}.lib`,
       },
     ],
